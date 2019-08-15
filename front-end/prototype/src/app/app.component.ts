b5e98c6e8eca16ef335e8d7d9cd56f4fa29a9ae0
Gabriel Quintana <gaq07ar@gmail.com>
2019-08-15 20:21:51 -0300

    Prototipo
    Una aproximación básica de como podría llegar a ser el front.

---
+++ front-end/prototype/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'proyecto Cavacuna';
+  temp1 = '17°';
+  temp2 = '8°';
+  temp3 = '12°';
+}
