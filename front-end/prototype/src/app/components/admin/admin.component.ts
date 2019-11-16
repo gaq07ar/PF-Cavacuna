e18b1baf3e56ff220c8a520070dfd38be20b5205
Nicolas <nicolasditloff@gmail.com>
2019-11-16 16:03:25 -0300

    arregle errores
    arregle errores en def de funcion bajaUsuarios

--- front-end/prototype/src/app/components/admin/admin.component.ts
+++ front-end/prototype/src/app/components/admin/admin.component.ts
@@ -9,19 +9,17 @@ import { Component, OnInit } from '@angular/core';
 
 export class AdminComponent implements OnInit {
 
-  public usuarios = ["Nico", "Gabi", "Dami", "Nacho"];
-  
+  public usuarios = ["Nico", "Gabi", "Dami", "Nacho",];
+    
   altaUsuario() {
-      let usuario= prompt("Enter a new item");
-      this.usuarios.push(usuario);
+    let usuario= prompt("Escriba el nombre del usuario nuevo");
+    this.usuarios.push(usuario);
   }
 
   bajaUsuario(nombre){
     for (var i = 0; i < this.usuarios.length; i++){
-      if (this.usuarios[i]["nombre"] == nombre){
+      if (this.usuarios[i] == nombre){
         this.usuarios.splice( i, 1 );
-        // console.log(this.usuarios);
-        let usuario= prompt("Escriba el nombre del usuario nuevo");
       }
     }
   }
