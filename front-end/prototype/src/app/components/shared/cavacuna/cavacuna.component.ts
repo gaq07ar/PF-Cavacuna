e6d411bab3caa85207efb388c629dd2d5466dfdb
Gabriel Quintana <gaq07ar@gmail.com>
2019-11-20 06:44:38 -0300

    Cambio de colores a las heladeras

--- front-end/prototype/src/app/components/shared/cavacuna/cavacuna.component.ts
+++ front-end/prototype/src/app/components/shared/cavacuna/cavacuna.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Device } from "../device.model";
 import { Slot } from "../slot.model";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: "app-cavacuna",
@@ -10,6 +12,11 @@ import { Slot } from "../slot.model";
 export class CavacunaComponent implements OnInit {
   @Input() device: Device;
   @Input() i: number;
+  actualTemperature: number;
+  isFetching = false;
+  acceptedRange: number[] = [];
+  minTemp: number;
+  maxTemp: number;
 
   slots: Slot[] = [
     new Slot(1, "Hepatitis B", false),
@@ -18,9 +25,59 @@ export class CavacunaComponent implements OnInit {
     new Slot(4, "Probando aplicada", true)
   ];
 
-  vacunas = ["vacu1", "vacu2", "vacu3","vacu4", "vacu5"];
+  vacunas = ["vacu1", "vacu2", "vacu3", "vacu4", "vacu5"];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.minTemp = Number(this.device.min_temp);
+    this.maxTemp = Number(this.device.max_temp);
+    let avg: number = (this.minTemp + this.maxTemp) / 2;
+    this.acceptedRange.push(avg - avg * 0.35);
+    this.acceptedRange.push(avg + avg * 0.35);
+    this.isFetching = true;
+    setInterval(() => this.fetchPosts(), 5000);
+  }
+
+  checkWarningValidation(incomingTemperature: number): boolean {
+    return (
+      (incomingTemperature >= this.minTemp &&
+        incomingTemperature <= this.acceptedRange[0]) ||
+      (incomingTemperature >= this.acceptedRange[1] &&
+        incomingTemperature <= this.maxTemp)
+    );
+  }
+
+  checkColdDangerValidation(incomingTemperature: number): boolean {
+    return incomingTemperature < this.minTemp;
+  }
+
+  checkHotDangerValidation(incomingTemperature: number): boolean {
+    return incomingTemperature > this.maxTemp;
+  }
+
+  private fetchPosts() {
+    this.http
+      .get(
+        "https://api.thingspeak.com/channels/" +
+          this.device.id +
+          "/fields/1/last.json?timezone=America%2FArgentina%2FBuenos_Aires"
+      )
+      .pipe(
+        map(
+          (responseData: {
+            created_at: string;
+            entry_id: string;
+            field1: number;
+          }) => {
+            return responseData.field1;
+          }
+        )
+      )
+      .subscribe(lastEntry => {
+        console.log(lastEntry);
+        this.actualTemperature = lastEntry;
+        this.isFetching = false;
+      });
+  }
 }
