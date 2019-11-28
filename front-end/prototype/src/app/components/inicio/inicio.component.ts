0b584bb6514d81d00690bd78bcc95976211b851c
Gabriel Quintana <gaq07ar@gmail.com>
2019-11-28 02:02:44 -0300

    Prueba

--- front-end/prototype/src/app/components/inicio/inicio.component.ts
+++ front-end/prototype/src/app/components/inicio/inicio.component.ts
@@ -24,6 +24,7 @@ export class InicioComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
+    //Comment
     this.isFetching = true;
     this.getPermissionInformation();
     this.processInitialData();
